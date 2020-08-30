2515209cd8d5e1b388840f29bd19ad962c90dff6
LYS0 <48874441+LYS0@users.noreply.github.com>
2020-08-31 01:09:09 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -9,9 +9,9 @@ build_dir = "../dist/"
 index_page_size = 10
 archives_page_size = 20
 template = {
-    "name": "Galileo",
+    "name": "long",
     "type": "local",
-    "path": "../Galileo"
+    "path": "../long"
 }
 enable_jsdelivr = {
     "enabled": False,
@@ -21,11 +21,11 @@ enable_jsdelivr = {
 # 站点设置
 site_name = "Long Long Ago"
 site_logo = "${static_prefix}logo.png"
-site_build_date = "2019-12-18T16:51+08:00"
+site_build_date = "2020-08-31T01:10+08:00"
 author = "龙颖诗～"
 email = "3077512847@qq.com"
 author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
+description = "至善至正。"
 key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
